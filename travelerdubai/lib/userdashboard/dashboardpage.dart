0b7415c43a70e065448b1edb4a979a139e457d4c
adityasinghborana <aditya@ogresto.com>
2024-03-27 17:33:32 +0530

    Dashboard page started

--- travelerdubai/lib/userdashboard/dashboardpage.dart
+++ travelerdubai/lib/userdashboard/dashboardpage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:travelerdubai/core/constants/contants.dart';
 import 'package:travelerdubai/core/widgets/header.dart';
+import 'package:travelerdubai/tourdetails/presentation/Widgets/button.dart';
 import 'package:travelerdubai/userdashboard/dashboard_controller.dart';
 import 'package:travelerdubai/userdashboard/widgets/Profile_info.dart';
 
@@ -9,66 +11,117 @@ class DashboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    final double width = MediaQuery.of(context).size.width;
+    final double Height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        children: [
-          Header(),
-          Expanded(
-            child: DefaultTabController(
-              length: 2,
-              child: Scaffold(
-                appBar: AppBar(
-                  title: Text('Dashboard'),
-                  bottom: TabBar(
-                    tabs: [
-                      Tab(text: 'Profile'),
-                      Tab(text: 'Bookings'),
-                    ],
-                    onTap: (index) {
-                      controller.selectedIndex.value = index;
-                      print(index);
-                    },
-                  ),
-                ),
-                body: GetBuilder<DashBoardController>(
 
-                  builder: (controller) => TabBarView(
-                    physics: NeverScrollableScrollPhysics(),
-                    children: [
-                      // First Tab: Profile
-                      Visibility(
-                        visible: controller.selectedIndex.value == 0,
-                        child: Container(
-                          height: 200,
-                          child: TextButton(
-                            child: Text("logout"),
-                            onPressed:() {
-                              controller.signOut();
-                            },
-
+      body: Container(
+        height: Height,
+        decoration: BoxDecoration(gradient: backgroundgradient),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Header(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.1),
+                child: ExpansionTile(
+                  leading: Icon(Icons.arrow_drop_down),
+                  trailing: null,
+          
+                  tilePadding: EdgeInsets.zero,
+                  // Optionally set padding to zero
+                  collapsedIconColor: Colors.black,
+                  // Set the color of the collapsed icon
+                  title: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3),
                           ),
-
-                        ),
+                        ],
                       ),
-
-                      // Second Tab: Bookings
-                      Visibility(
-                        visible: controller.selectedIndex.value ==1,
-                        child: Container(
-                          height: 200,
-                          child: ProfileInfo(),
-                        ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(width: 8), // Adjust spacing as needed
+                              Text("Account"),
+                            ],
+                          ),
+                          InlineFlexButton(
+                            label: "Save",
+                            bgcolor: Colors.blue,
+                            onPressed: () => print("hello"),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
+                  children: [
+                    // Content that will be displayed when the tile is expanded
+                    ProfileInfo(),
+                  ],
                 ),
               ),
-            ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.1),
+                child: ExpansionTile(
+                  leading: Icon(Icons.arrow_drop_down),
+                  trailing: null,
+          
+                  tilePadding: EdgeInsets.zero,
+                  // Optionally set padding to zero
+                  collapsedIconColor: Colors.black,
+                  // Set the color of the collapsed icon
+                  title: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(width: 8), // Adjust spacing as needed
+                              Text("Bookings"),
+                            ],
+                          ),
+                          InlineFlexButton(
+                            label: "Save",
+                            bgcolor: Colors.blue,
+                            onPressed: () => print("hello"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  children: [
+                    // Content that will be displayed when the tile is expanded
+                    ProfileInfo(),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
-
 }
