346b39c3b80a83cbd423693fc61187b06f5cf329
adityasinghborana <aditya@ogresto.com>
2024-04-15 05:52:25 +0530

    merged with vktourdetails

--- travelerdubai/lib/tourdetails/presentation/screen/desktop/tours_screen_desktop.dart
+++ travelerdubai/lib/tourdetails/presentation/screen/desktop/tours_screen_desktop.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +9,7 @@ import 'package:travelerdubai/Cart/data_layer/repository/cart_repository.dart';
 import 'package:travelerdubai/Cart/data_layer/service/cart_remote.dart';
 import 'package:travelerdubai/Cart/data_layer/usecase/update_cart.dart';
 import 'package:travelerdubai/Components/date_picker.dart';
+import 'package:travelerdubai/Components/ui_state.dart';
 import 'package:travelerdubai/core/constants/constants.dart';
 import 'package:travelerdubai/core/widgets/header.dart';
 import 'package:travelerdubai/homepage/presentaion/widgets/cities.dart';
@@ -33,6 +36,11 @@ import '../../Widgets/tranfertype_dropdown.dart';
 
 class TourPageDesktop extends StatelessWidget {
   TourPageDesktop({super.key});
+  final TourController tourController = Get.put(TourController(
+    GetCityTourUseCase(TourRepositoryImpl(TourRemoteService(Dio()))),
+  ));
+  final HeaderController controller = Get.find();
+
   final TourOptionStaticDataController static = Get.put(
     TourOptionStaticDataController(
         GetTourOptionsStaticDataUseCase(
@@ -53,36 +61,31 @@ class TourPageDesktop extends StatelessWidget {
           ),
         )),
   );
-  final HeaderController controller = Get.find();
-  final TourController tourController = Get.put(TourController(
-    GetCityTourUseCase(TourRepositoryImpl(TourRemoteService(Dio()))),
-  ));
+
   @override
   Widget build(BuildContext context) {
-    final TourOptionStaticDataController static = Get.put(
-      TourOptionStaticDataController(
-          GetTourOptionsStaticDataUseCase(
-              TourOptionsRepositoryImpl(TourOptionRemoteService(Dio()))),
-          GetTourOptionsDynamicDataUseCase(
-            TourOptionsRepositoryImpl(
-              TourOptionRemoteService(Dio()),
-            ),
-          ),
-          GetTimeSlotUseCase(
-            TimeSlotRepositoryImpl(
-              TimeSlotRemoteService(Dio()),
-            ),
-          ),
-          UpdateCartUseCase(
-            CartRepositoryImpl(
-              CartRemoteService(Dio()),
-            ),
-          )),
-    );
-
+    static.dateTextController.value.text = DateTime.now()
+        .add(
+      // Add a duration representing the specified number of hours.
+      Duration(hours: tourController.tour.value.cutOffhrs ?? 0),
+    )
+        .toString()
+        .substring(0, 10);
+    static.selectedDate.value = DateTime.now().add(
+      // Add a duration representing the specified number of hours.
+        Duration(hours: tourController.tour.value.cutOffhrs ?? 0));
     if (kDebugMode) {
-      print("${controller.cartId.value} hello tour deatial");
+      print("${controller.cartId.value} hello tour detail");
     }
+    ever(tourController.isLoading, (isLoading) {
+      if (!isLoading) {
+        static.id.value = tourController.tour.value.TourId.toString();
+        static.contractid.value =
+            tourController.tour.value.contractId.toString();
+        static.getOptionsStaticData();
+      }
+    });
+
     //final double Width = MediaQuery.of(context).size.width;
     return Scaffold(
       floatingActionButton: ElevatedButton(
@@ -90,28 +93,20 @@ class TourPageDesktop extends StatelessWidget {
         onPressed: () => Get.toNamed("/checkout"),
       ),
       body: Obx(
-        () {
+            () {
+          print('in the body obx');
           if (tourController.isLoading.isTrue) {
             return const Center(child: CircularProgressIndicator());
           } else {
-            static.id.value=tourController.tour.value.TourId.toString();
-            static.contractid.value =
-                tourController.tour.value.contractId.toString();
-            static.dateTextController.value.text = DateTime.now()
-                .add(
-                    // Add a duration representing the specified number of hours.
-                    Duration(hours: tourController.tour.value.cutOffhrs ?? 0))
-                .toString()
-                .substring(0, 10);
-            static.selectedDate.value = DateTime.now().add(
-                // Add a duration representing the specified number of hours.
-                Duration(hours: tourController.tour.value.cutOffhrs ?? 0));
-            static.getOptionsStaticData();
-
+            if (kDebugMode) {
+              print(
+                  'in the obx the options stats is ${static.options.value.state}');
+            }
             var tourImages = tourController.tourImages;
             List<String> imageUrls =
-                tourImages.map((imageModel) => imageModel.imagePath!).toList();
+            tourImages.map((imageModel) => imageModel.imagePath!).toList();
 
+            var optionsOutput = static.options.value.state;
             return SingleChildScrollView(
               child: Container(
                 decoration: BoxDecoration(gradient: backgroundgradient),
@@ -126,6 +121,7 @@ class TourPageDesktop extends StatelessWidget {
                           images: imageUrls,
                           maxWidth: double.infinity,
                           height: MediaQuery.of(context).size.height * 0.5,
+                          showButtons: false,
                         ),
                         Padding(
                           padding: EdgeInsets.symmetric(
@@ -143,7 +139,7 @@ class TourPageDesktop extends StatelessWidget {
 
                         Padding(
                           padding:
-                              EdgeInsets.symmetric(vertical: Get.height * 0.01),
+                          EdgeInsets.symmetric(vertical: Get.height * 0.01),
                           child: Row(
                             children: [
                               Padding(
@@ -151,7 +147,7 @@ class TourPageDesktop extends StatelessWidget {
                                     horizontal: Get.width * 0.05),
                                 child: Row(
                                   mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
+                                  MainAxisAlignment.spaceBetween,
                                   children: [
                                     IconTextBackground(
                                         iconData: Icons.remove_red_eye,
@@ -185,7 +181,7 @@ class TourPageDesktop extends StatelessWidget {
                                     iconData: Icons.audiotrack,
                                     text: 'Audio Guide',
                                     backgroundColor:
-                                        const Color.fromRGBO(0, 154, 184, 0.20),
+                                    const Color.fromRGBO(0, 154, 184, 0.20),
                                     iconColor: color_088943,
                                     textStyle: iconText.copyWith(fontSize: 16)),
                               ),
@@ -197,7 +193,7 @@ class TourPageDesktop extends StatelessWidget {
                     ),
                     Padding(
                       padding:
-                          EdgeInsets.symmetric(horizontal: Get.width * 0.05),
+                      EdgeInsets.symmetric(horizontal: Get.width * 0.05),
                       child: Row(
                         children: [
                           Flexible(
@@ -223,86 +219,96 @@ class TourPageDesktop extends StatelessWidget {
   }
 
   Widget formSection() {
-    final TourOptionStaticDataController static = Get.put(
-      TourOptionStaticDataController(
-          GetTourOptionsStaticDataUseCase(
-              TourOptionsRepositoryImpl(TourOptionRemoteService(Dio()))),
-          GetTourOptionsDynamicDataUseCase(
-            TourOptionsRepositoryImpl(
-              TourOptionRemoteService(Dio()),
-            ),
-          ),
-          GetTimeSlotUseCase(
-            TimeSlotRepositoryImpl(
-              TimeSlotRemoteService(Dio()),
-            ),
-          ),
-          UpdateCartUseCase(
-            CartRepositoryImpl(
-              CartRemoteService(Dio()),
-            ),
-          )),
-    );
-    final HeaderController controller = Get.put(HeaderController());
-    final TourController tourController = Get.put(TourController(
-      GetCityTourUseCase(TourRepositoryImpl(TourRemoteService(Dio()))),
-    ));
-    return Card(
-      elevation: 25.0,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Container(
-          padding: const EdgeInsets.all(50.0),
-          color: colorwhite,
-          width: Get.width * 0.9,
-          height: Get.height * .5,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Obx(() => DropdownWidget(
-                      label: 'Adults',
-                      selectedValue: static.adultsSelectedValue.value,
-                      onChanged: (value) {
-                        static.adultsSelectedValue.value = value ?? 1;
-                        static.getOptionsdynamicData();
-                      })),
-                  Obx(() => DropdownWidget(
-                      label: 'Children',
-                      selectedValue: static.childrenSelectedValue.value,
-                      onChanged: (value) {
-                        static.childrenSelectedValue.value = value ?? 0;
-                        static.getOptionsdynamicData();
-                      })),
-                  Obx(() => DropdownWidget(
-                      label: 'Infants',
-                      selectedValue: static.infantsSelectedValue.value,
-                      onChanged: (value) {
-                        static.infantsSelectedValue.value = value ?? 0;
-                        static.getOptionsdynamicData();
-                      })),
-                  dateInputField(static.dateTextController.value, Get.context!,
-                      () {
-                    static.selectedDate.value =
-                        DateTime.parse(static.dateTextController.value.text);
-                    static.getOptionsdynamicData();
-                    static.gettimeSlots();
-                  }, null),
-                  DropdownTransferWidget(
+    HashMap<String, int> transferOptionsMap = HashMap<String, int>();
+    if (kDebugMode) {
+      print('static.dynamic length is ${static.dynamicoptions.length}');
+      print(
+          'in the form section optins state is ${static.options.value.state}');
+    }
+
+    if (static.dynamicoptions.isNotEmpty) {
+      static.dynamicoptions.forEach((tourOptionsDynamicResult) {
+        // Assuming tourOptionsDynamicResult.transferName and tourOptionsDynamicResult.transferId are not null
+        static.addUniquePair(
+          transferOptionsMap,
+          tourOptionsDynamicResult.transferName!,
+          tourOptionsDynamicResult.transferId!,
+        );
+      });
+      static.selectedTransfer.value = static.dynamicoptions[0].transferName!;
+    }
+    if (kDebugMode) {
+      print('transferOptionMap is$transferOptionsMap');
+    }
+    return Obx(() {
+      return Card(
+        elevation: 25.0,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            padding: const EdgeInsets.all(50.0),
+            color: colorwhite,
+            width: Get.width * 0.9,
+            height: Get.height * .5,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Obx(() => DropdownWidget(
+                        label: 'Adults',
+                        selectedValue: static.adultsSelectedValue.value,
+                        onChanged: (value) {
+                          static.adultsSelectedValue.value = value ?? 1;
+                          static.getOptionsDynamicData();
+                        })),
+                    Obx(() => DropdownWidget(
+                        label: 'Children',
+                        selectedValue: static.childrenSelectedValue.value,
+                        onChanged: (value) {
+                          static.childrenSelectedValue.value = value ?? 0;
+                          static.getOptionsDynamicData();
+                        })),
+                    Obx(() => DropdownWidget(
+                        label: 'Infants',
+                        selectedValue: static.infantsSelectedValue.value,
+                        onChanged: (value) {
+                          static.infantsSelectedValue.value = value ?? 0;
+                          //  static.getOptionsDynamicData();
+                        })),
+
+                    Obx(() {
+                      return dateInputField(
+                          static.dateTextController.value, Get.context!, () {
+                        static.changePickedDate(DateTime.parse(
+                            static.dateTextController.value.text));
+
+                        //static.getTimeSlots();
+                      }, null);
+                    }),
+                    DropdownTransferWidget(
                       label: 'type',
                       selectedValue: static.selectedTransfer.value,
-                      onChanged: (value) => static.changeSelectedTransfer),
-                ],
-              ),
-              const Divider(
-                height: 1,
-              ),
-              options(tourController.tour.value.tourName!),
-            ],
+                      // onChanged: (value) {
+                      //   static.selectedTransfer.value = value!;
+                      //
+                      // },
+                      onChanged: (value) {
+                        static.changeSelectedTransfer(value);
+                      },
+                      items: transferOptionsMap.keys.toList(),
+                    ),
+                  ],
+                ),
+                const Divider(
+                  height: 1,
+                ),
+                options(tourController.tour.value.tourName!),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
